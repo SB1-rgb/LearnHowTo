39b94b48120d61b821f41c32ca88985ffc454617
SB1-rgb <125292154+SB1-rgb@users.noreply.github.com>
2024-09-17 16:44:37 +0300

    one new boolean variable added

---
+++ src/main/java/md/java/variables/CommentsAndVariables.java
@@ -0,0 +1,8 @@
+package md.java.variables;
+
+public class CommentsAndVariables {
+    public static void main(String[] args) {
+        boolean isHotOutside= true;
+        System.out.println(isHotOutside);
+    }
+}
