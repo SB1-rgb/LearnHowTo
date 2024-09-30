0347383722a491de7977d7dc0b912701afcf1ab3
SB1-rgb <125292154+SB1-rgb@users.noreply.github.com>
2024-09-30 11:10:21 +0300

    added few clases and objects

---
+++ src/main/java/md/java/classesandobjects/autoservicetask/Tool.java
@@ -0,0 +1,12 @@
+package md.java.classesandobjects.autoservicetask;
+
+public class Tool {
+
+    String nameOfTheTool;
+    String material;
+
+    public Tool(String name){
+        this.nameOfTheTool = name;
+        System.out.println("this tool has only one param");
+    }
+}
---
+++ src/main/java/md/java/classesandobjects/autoservicetask/Worker.java
@@ -0,0 +1,13 @@
+package md.java.classesandobjects.autoservicetask;
+
+public class Worker {
+    String name;
+    int experience;
+    int pricePerHour;
+
+
+    public Worker(String nameOfTheWorker){
+        this.name =nameOfTheWorker;
+        System.out.println("only the name is known of the worker");
+    }
+}
